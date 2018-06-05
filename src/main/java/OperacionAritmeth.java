199cef6cc2f42f1e07917bcb84a63baad7e7264b
Camilo <camihu8@gmail.com>
2018-06-05 08:49:24 -0400

    suma camilo

--- src/main/java/OperacionAritmeth.java
+++ src/main/java/OperacionAritmeth.java
@@ -2,4 +2,12 @@
  * Created by Camilo on 05/06/2018.
  */
 public class OperacionAritmeth {
+
+    public static void main (String[] args) {
+        int a = 1;
+        int b = 2;
+        int suma = a + b;
+        System.out.print("la suma es:");
+        System.out.print(suma);
+    }
 }
