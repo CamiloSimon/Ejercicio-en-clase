1d26764413901a79f81d4834c1826407912a3dcc
Camilo <camihu8@gmail.com>
2018-06-05 08:43:22 -0400

    resta Ani

--- src/main/java/OperacionAritmeth.java
+++ src/main/java/OperacionAritmeth.java
@@ -2,4 +2,12 @@
  * Created by Camilo on 05/06/2018.
  */
 public class OperacionAritmeth {
+
+    public static void main(){
+        int i =10;
+        int j=2;
+        int resta=i - j;
+          System.out.print("Resta:");
+          System.out.println(resta);
+}
 }
