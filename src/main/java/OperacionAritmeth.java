eb5c2dfbe2d7f133d41e50d6cd01e8ed921e1a8f
HFFY <hugorilahffy@gmail.com>
2018-06-05 08:47:31 -0400

    tarea terminada

--- src/main/java/OperacionAritmeth.java
+++ src/main/java/OperacionAritmeth.java
@@ -2,13 +2,9 @@
  * Created by Camilo on 05/06/2018.
  */
 public class OperacionAritmeth {
+    public int multiplicacion(int a, int b){
+        return a*b;
+    }
+
 }
 
-public int multiplicacion(int a, int b){
-    return a*b;
-}
-
-@Test
-public void testMultiplicacion(){
-    Assert.IsEqual(multiplicacion(5, 3), 15);
-}
